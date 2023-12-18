bbc45d9f339d0bc23165bc4d7a50bd9df4ef5570
AlexLobach <alexlobatch@yandex.ru>
2023-12-18 19:07:41 +0300

    changes before pooling

--- Assets/Scripts/Cannonball/CannonballShots.cs
+++ Assets/Scripts/Cannonball/CannonballShots.cs
@@ -9,23 +9,17 @@ public class CannonballShots : MonoBehaviour
     public GameObject cannonball_abs;
     [SerializeField]
     private GameObject cannon;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.V))
         {
             float force = Random.Range(1200, 2000);
-            GameObject cannonball = Instantiate(cannonball_abs) as GameObject;
+            GameObject cannonball = Instantiate(cannonball_abs);
             cannonball.transform.position = cannon.transform.position;
             cannonball.transform.rotation = cannon.transform.rotation;
             cannonball.transform.Translate(0,0.75f,1f);
             cannonball.GetComponent<Rigidbody>().AddRelativeForce(cannonball.transform.forward * force, ForceMode.Force);
-
         }
 
     }
