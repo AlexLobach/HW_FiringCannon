29e57ae8184af49ddb8750cf66766415da6e730b
AlexLobach <alexlobatch@yandex.ru>
2023-12-19 18:52:53 +0300

    rotation restriction (bad realization)

--- Assets/Scripts/Cannon/RotateBase.cs
+++ Assets/Scripts/Cannon/RotateBase.cs
@@ -17,7 +17,8 @@ public class  RotateBase : RotateAxis
 
     void ApplyRotation()
     {
-        axis.rotation = Quaternion.Euler(current.y, current.x, 0);    
+       // axis.rotation = Quaternion.Euler(current.y, current.x, 0) ;
+        axis.rotation = Quaternion.Euler(Mathf.Clamp(current.y , -15, 15), Mathf.Clamp(current.x, -15, 15), 0) ;    
     }   
             
     public override void Rotate(Vector2 vector)
