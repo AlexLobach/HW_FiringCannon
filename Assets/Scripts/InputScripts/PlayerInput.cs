6cd2b6eb321208111305babb7f91a2c59e7cb3fb
AlexLobach <alexlobatch@yandex.ru>
2023-12-19 18:57:35 +0300

    modified  PlayerInput

--- Assets/Scripts/InputScripts/PlayerInput.cs
+++ Assets/Scripts/InputScripts/PlayerInput.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] 
-    private RotateBase rotateBase;
+    private ReducerAxis reducerAxis;
 
     public void Update() 
     {
-        rotateBase.Rotate(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime*20);
+        reducerAxis.Rotate(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime*20);
     }
 }
